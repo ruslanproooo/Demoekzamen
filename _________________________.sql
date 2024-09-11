2c04940c1cdc56aaaa698f26ad63917cb39dd062
ruslanproooo <144116019+ruslanproooo@users.noreply.github.com>
2024-09-11 13:23:31 +0300

    Add files via upload

---
+++ _________________________.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost
+-- Время создания: Сен 11 2024 г., 12:23
+-- Версия сервера: 10.4.32-MariaDB
+-- Версия PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `Нарушений Нет`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Нарушений Нет`
+--
+-- Структура чтения ошибок для таблицы Нарушений Нет.Нарушений Нет: #1146 - Таблица &#039;Нарушений Нет.Нарушений Нет&#039; не существует
+-- Ошибка считывания данных таблицы Нарушений Нет.Нарушений Нет: #1064 - У вас ошибка в запросе. Изучите документацию по используемой версии MariaDB на предмет корректного синтаксиса около &#039;FROM `Нарушений Нет`.`Нарушений Нет`&#039; на строке 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
